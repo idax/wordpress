a91b89397ebee23e4667febd412580849525cd61
Unknown <harbo600@gmail.com>
2018-09-24 21:34:49 +0200

    social media linkedin

--- themes/shop-isle/footer.php
+++ themes/shop-isle/footer.php
@@ -8,6 +8,7 @@
  * @subpackage Shop Isle
  */
 ?>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 <?php do_action( 'shop_isle_before_footer' ); ?>
 
 	<?php do_action( 'shop_isle_footer' ); ?>
